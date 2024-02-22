6b12c9bf006987ba2c553f978d1a2110fbd5477f
Nick Cheng <llc564978@gmail.com>
2024-02-22 09:15:50 +0800

    feat: remove comments

--- app.py
+++ app.py
@@ -11,12 +11,9 @@ import os
 
 app = Flask(__name__)
 
-# Channel Access Token
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
 handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
 
-# 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -31,7 +28,6 @@ def callback():
         abort(400)
     return 'OK'
 
-# 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
